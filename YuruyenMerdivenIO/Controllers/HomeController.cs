611ce6fde7d717f69479557e0590ef7d366cff67
cagdasbilgin <cagdasb96@gmail.com>
2018-08-23 22:08:06 +0300

    local dosyalar yüklendi

---
+++ YuruyenMerdivenIO/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace YuruyenMerdivenIO.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            List<SelectListItem> derece = new List<SelectListItem>();
+            for(int i=0; i<=7; i ++)
+            {
+                derece.Add(new SelectListItem { Text = (i * 5).ToString(), Value = i.ToString() });
+            }
+            ViewBag.Derece = derece;
+            //derece.Add(new SelectListItem { Text = "0", Value = "0" });
+            return View();
+        }
+    }
+}
---
+++ YuruyenMerdivenIO/Models/YuruyenMerdivenModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace YuruyenMerdivenIO.Models
+{
+    public class YuruyenMerdivenModel
+    {
+        public string Yukseklik { get; set; }
+        public string Derece { get; set; }
+        public string BasamakGenisligi { get; set; }
+    }
+}
